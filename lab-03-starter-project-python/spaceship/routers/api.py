b19eac5af0c12acaaef96e62fc756e41ca314755
stormy131 <artemkopyl1990@gmail.com>
2022-04-24 13:21:02 +0300

    implemented endpoint with NumPy usage

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def get_matrix():
+    matrix_a = np.random.rand(10,10)
+    matrix_b = np.random.rand(10,10)
+    product = np.matmul(matrix_a, matrix_b)
+
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': product.tolist()
+    }
